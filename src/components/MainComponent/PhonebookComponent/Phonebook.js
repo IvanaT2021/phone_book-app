f8dda024d874ffb3cc8d28e6985ee88ceaf8c570
unknown <aliceinkland@gmail.com>
2022-02-03 00:25:13 +0100

    adding functionality and some css

--- src/components/MainComponent/PhonebookComponent/Phonebook.js
+++ src/components/MainComponent/PhonebookComponent/Phonebook.js
@@ -1,13 +1,37 @@
-import React from "react";
-import styles from "./Phonebook.module.css";
-import Book from "./BookComponent/Book";
-import EntryForm from "./EntryFormComponent/EntryForm";
+import React, { useState } from 'react';
+import styles from './Phonebook.module.css';
+import Book from './BookComponent/Book'
 
-const Phonebook = () => {
+const Phonebook = (props) => {
   return (
     <div className={styles.PhoneBook}>
-      <Book/>
-      <EntryForm />
+      
+      <table>
+        <thead>
+          <tr>
+            <th>Ime</th>
+            <th>Prezime</th>
+            <th>Email</th>
+            <th>Broj Telefona</th>
+            <th>Izmjene</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.contactPropsFromMain.map((contact) => (
+            <Book
+            contact={contact}
+            key={contact.id}
+           
+            setEditForm={props.setEditForm}
+            //handleFormFromPBook={props.handleFormFromMain}
+            setUpdate={props.setUpdateFromMain}
+            handleDeleteFromPBook={props.handleDeleteFromMain}
+            />
+          ))}
+          
+        </tbody>
+      </table>
+     
     </div>
   );
 };
